234e04d43ef6f8d19b2c08ed20c749fbf9b45e19
Volodymyr <volodyaruzhak@gmail.com>
2023-04-09 15:41:08 -0400

    add tests, create greed component

--- src/__tests__/Greed.test.tsx
+++ src/__tests__/Greed.test.tsx
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react'
 import Greed from "../components/Greed/Greed"
 
-test('greed renders correctly', () => {
+test('greed renders correctly, first test', () => {
     render(<Greed />)
-    const textElement = screen.getByText('Greed');
+    const textElement = screen.getByText(/greed/i);
     expect(textElement).toBeInTheDocument();
+})
+
+// greet should rnder hello and if name is passed into the component
+// it should render hello followed by component
+
+test('greed renders correctly, second test', () => {
+    render(<Greed />)
+    const textElement = screen.getByText(/hello/i)
+    expect(textElement).toBeInTheDocument();
+})
+
+test('greet render with a name', ()=> {
+    render(<Greed name='Volodymyr' />)
+    const textElement = screen.getByText(/hello volodymyr/i)
+    expect(textElement).toBeInTheDocument()
 })
